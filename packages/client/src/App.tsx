0b96d0595553fed1691f75889878393c4d693be1
will <will@penguin>
2019-12-29 11:45:59 -0500

    [client] setup apollo client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { ApolloProvider } from "react-apollo";
+import { useApolloClient } from "utils/apollo-client";
 
 const App: React.FC = () => {
-  return <div>Hello Wold</div>;
+  const client = useApolloClient();
+  if (!client) return null;
+  return (
+    <ApolloProvider client={client}>
+      <div>Hello Wold</div>
+    </ApolloProvider>
+  );
 };
 
 export default App;
