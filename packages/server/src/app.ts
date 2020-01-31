61438a1804aa5604f949553c8d4256828b64a891
will <will@penguin>
2020-01-31 07:32:17 -0500

    [server][db] connect db module to main app

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,9 +1,11 @@
 import { GraphQLModule } from "@graphql-modules/core";
 import gql from "graphql-tag";
 import DocumentModule from "document-module";
+import DatabaseModule from "db";
+import { DATABASE_URL } from "./env";
 
 export const App = new GraphQLModule({
-  imports: [DocumentModule],
+  imports: [DocumentModule, DatabaseModule.forRoot({ DB_URL: DATABASE_URL })],
   typeDefs: gql`
     type Query {
       status: String
