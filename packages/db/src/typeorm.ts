91a93ee04900798e5da5b3998346c53c777c800b
will <will@penguin>
2020-02-06 17:34:41 -0500

    [db] specify entities for typeorm connector

--- packages/db/src/typeorm.ts
+++ packages/db/src/typeorm.ts
@@ -1,11 +1,15 @@
 import { createConnection } from "typeorm";
 export { getConnection, Connection } from "typeorm";
+import { Collection } from "./entities/collection";
+import { Thing } from "./entities/thing";
+import { User } from "./entities/user";
 
 type Props = { url: string } & { [key: string]: any };
 
 export const setupConnection = async (props: Props) => {
   return await createConnection({
     type: "postgres",
+    entities: [Collection, Thing, User],
     ...props
   });
 };
