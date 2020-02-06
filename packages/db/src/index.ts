fab3f4bcf811c72538373e720e9e7da3d0171695
will <will@penguin>
2020-02-06 17:36:17 -0500

    [db] init db module

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,95 @@
-import { GraphQLModule } from "@graphql-modules/core";
+import { GraphQLModule, ModuleContext } from "@graphql-modules/core";
 import gql from "graphql-tag";
+import { UserProvider } from "./entities/user";
+import { CollectionProvider } from "./entities/collection";
+import { ThingProvider } from "./entities/thing";
+import { setupConnection } from "./typeorm";
 
-const DatabaseModule = new GraphQLModule({
-  typeDefs: gql``,
-  resolvers: {},
-  providers: []
+const DatabaseModule = new GraphQLModule<
+  { DB_URL: string },
+  any,
+  ModuleContext
+>({
+  typeDefs: [
+    gql`
+      type User {
+        id: ID!
+        collections: [Collection!]!
+      }
+      type Collection {
+        id: ID!
+        things: [Thing!]!
+      }
+      type Thing {
+        id: ID!
+      }
+      type Mutation {
+        users: UserMutations!
+        collections: CollectionMutations!
+        things: ThingMutations!
+      }
+      type Query {
+        users: UserQueries!
+      }
+      type UserQueries {
+        user(id: ID!): User
+      }
+      type UserMutations {
+        create: User
+      }
+      type CollectionMutations {
+        create(userId: ID!): Collection
+      }
+      type ThingMutations {
+        create(collectionId: ID!): Thing
+      }
+    `
+  ],
+  resolvers: {
+    Query: {
+      users: () => ({})
+    },
+    UserQueries: {
+      user: (_, { id }, { injector }) => injector.get(UserProvider).getUser(id)
+    },
+    User: {
+      collections: ({ id }, _, { injector }) =>
+        injector.get(UserProvider).getCollections(id)
+    },
+    Collection: {
+      things: ({ id }, _, { injector }) =>
+        injector.get(CollectionProvider).getThings(id)
+    },
+    Mutation: {
+      users: () => ({}),
+      collections: () => ({}),
+      things: () => ({})
+    },
+    UserMutations: {
+      create: async (_, __, { injector }) =>
+        injector.get(UserProvider).createUser()
+    },
+    CollectionMutations: {
+      create: async (_, { userId }, { injector }) =>
+        injector
+          .get(CollectionProvider)
+          .createCollection(await injector.get(UserProvider).getUser(userId))
+    },
+    ThingMutations: {
+      create: async (_, { collectionId }, { injector }) =>
+        injector
+          .get(ThingProvider)
+          .createThing(
+            await injector.get(CollectionProvider).getCollection(collectionId)
+          )
+    }
+  },
+  providers: ({ config: { DB_URL } }) => {
+    setupConnection({
+      url: DB_URL
+    });
+    return [UserProvider, CollectionProvider, ThingProvider];
+  }
 });
 
 export default DatabaseModule;
