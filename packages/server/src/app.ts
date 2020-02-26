6d506c94f40beecbf306458ce4d971ab60924219
will <will@penguin>
2020-02-26 18:40:58 -0500

    [server] connect auth module

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -2,10 +2,24 @@ import { GraphQLModule } from "@graphql-modules/core";
 import gql from "graphql-tag";
 import DocumentModule from "document-module";
 import DatabaseModule from "db";
-import { DATABASE_URL } from "./env";
+import AuthModule from "auth-module";
+import {
+  DATABASE_URL,
+  GOOGLE_AUTH_ID,
+  GOOGLE_AUTH_SECRET,
+  GOOGLE_AUTH_CALLBACK
+} from "./env";
 
 export const App = new GraphQLModule({
-  imports: [DocumentModule, DatabaseModule.forRoot({ DB_URL: DATABASE_URL })],
+  imports: [
+    DocumentModule,
+    DatabaseModule.forRoot({ DB_URL: DATABASE_URL }),
+    AuthModule.forRoot({
+      googleAuthId: GOOGLE_AUTH_ID,
+      googleAuthSecret: GOOGLE_AUTH_SECRET,
+      googleAuthCallback: GOOGLE_AUTH_CALLBACK
+    })
+  ],
   typeDefs: gql`
     type Query {
       status: String
--- packages/server/src/env.ts
+++ packages/server/src/env.ts
@@ -8,3 +8,6 @@ export const SERVER_BASE = process.env.SERVER_BASE!;
 export const SERVER_GRAPHQL_ENDPOINT = process.env.SERVER_GRAPHQL_ENDPOINT!;
 export const SERVER_BASE_URL = `${SERVER_BASE}:${SERVER_PORT}`;
 export const DATABASE_URL = process.env.DATABASE_URL!;
+export const GOOGLE_AUTH_ID = process.env.GOOGLE_AUTH_ID!;
+export const GOOGLE_AUTH_SECRET = process.env.GOOGLE_AUTH_SECRET!;
+export const GOOGLE_AUTH_CALLBACK = process.env.GOOGLE_AUTH_CALLBACK!;
