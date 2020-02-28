d20353ef051d917acbadc11aa4a47ed8ea736e64
will <will@penguin>
2020-02-28 16:56:43 -0500

    [db] setupConnection doesn't return a connection

--- packages/db/src/run-migrations.ts
+++ packages/db/src/run-migrations.ts
@@ -1,13 +1,14 @@
-import { setupConnection } from "./typeorm";
+import { setupConnection, getConnection } from "./typeorm";
 import { DATABASE_URL } from "./env";
 
 (async () => {
-  const connection = await setupConnection({
+  await setupConnection({
     entities: ["src/entities/**/*.ts"],
     migrations: ["src/migration/**/*.ts"],
     subscribers: ["src/subscriber/**/*.ts"],
     url: DATABASE_URL
   });
+  const connection = getConnection();
 
   await connection.runMigrations({
     transaction: "all"
