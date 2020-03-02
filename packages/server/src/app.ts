9fe883b4c7dd3a3b63dfaa2855e8a4a7579b8716
will <will@penguin>
2020-03-02 00:01:51 -0500

    [server] connect [aws] module

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -3,8 +3,12 @@ import gql from "graphql-tag";
 import DocumentModule from "document-module";
 import DatabaseModule from "db";
 import AuthModule from "auth-module";
+import AWSModule from "aws-module";
 import {
   DATABASE_URL,
+  AWS_USER,
+  AWS_PASSWORD,
+  AWS_S3_BUCKET,
   GOOGLE_AUTH_ID,
   GOOGLE_AUTH_SECRET,
   GOOGLE_AUTH_CALLBACK
@@ -14,6 +18,11 @@ export const App = new GraphQLModule({
   imports: [
     DocumentModule,
     DatabaseModule.forRoot({ DB_URL: DATABASE_URL }),
+    AWSModule.forRoot({
+      accessKeyId: AWS_USER,
+      secretAccessKey: AWS_PASSWORD,
+      bucketName: AWS_S3_BUCKET
+    }),
     AuthModule.forRoot({
       googleAuthId: GOOGLE_AUTH_ID,
       googleAuthSecret: GOOGLE_AUTH_SECRET,
