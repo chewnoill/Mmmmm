e9c84199f572c5c8f734f95406731d64f6bfdb80
will <will@penguin>
2020-03-02 00:07:50 -0500

    [db] remove mutations and root queries from db module

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -38,39 +38,9 @@ const DatabaseModule = new GraphQLModule<
         id: ID!
         value: String!
       }
-      type Mutation {
-        users: UserMutations!
-        collections: CollectionMutations!
-        things: ThingMutations!
-      }
-      type Query {
-        users: UserQueries!
-      }
-      type UserQueries {
-        user(id: ID!): User
-      }
-      type UserMutations {
-        create: User
-      }
-      input CreateCollectionsInput {
-        userId: ID!
-        name: String!
-      }
-      type CollectionMutations {
-        create(input: CreateCollectionsInput!): Collection
-      }
-      type ThingMutations {
-        create(collectionId: ID!): Thing
-      }
     `
   ],
   resolvers: {
-    Query: {
-      users: () => ({})
-    },
-    UserQueries: {
-      user: (_, { id }, { injector }) => injector.get(UserProvider).getUser(id)
-    },
     User: {
       collections: ({ id }, _, { injector }) =>
         injector.get(UserProvider).getCollections(id)
@@ -78,32 +48,6 @@ const DatabaseModule = new GraphQLModule<
     Collection: {
       things: ({ id }, _, { injector }) =>
         injector.get(CollectionProvider).getThings(id)
-    },
-    Mutation: {
-      users: () => ({}),
-      collections: () => ({}),
-      things: () => ({})
-    },
-    UserMutations: {
-      create: async (_, __, { injector }) =>
-        injector.get(UserProvider).createUser()
-    },
-    CollectionMutations: {
-      create: async (_, { userId, name }, { injector }) =>
-        injector
-          .get(CollectionProvider)
-          .createCollection(
-            await injector.get(UserProvider).getUser(userId),
-            name
-          )
-    },
-    ThingMutations: {
-      create: async (_, { collectionId }, { injector }) =>
-        injector
-          .get(ThingProvider)
-          .createThing(
-            await injector.get(CollectionProvider).getCollection(collectionId)
-          )
     }
   },
   providers: [UserProvider, CollectionProvider, ThingProvider]
