d6c6884b8c34ad8abf3740eba752dce444e6c14e
will cohen <willc@mojotech.com>
2020-04-18 11:34:44 -0400

    [client] add create collection and thing buttons

--- packages/client/src/pages/main.tsx
+++ packages/client/src/pages/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { useGetMyProfileQuery } from "codegen";
+import {
+  useGetMyProfileQuery,
+  useCreateCollectionMutation,
+  GetMyProfileDocument
+} from "codegen";
 import { Link } from "react-router-dom";
 
 export default () => {
   const { data } = useGetMyProfileQuery();
+  const [createCollection] = useCreateCollectionMutation();
   if (!data || !data.me || !data.me.user) return <div>loading...</div>;
 
   return (
@@ -12,6 +17,18 @@ export default () => {
       {data.me.user.collections.edges.map(({ edge: { id, name } }) => (
         <Link to={`/collection/${id}`}>{name}</Link>
       ))}
+      <button
+        onClick={() =>
+          createCollection({
+            variables: {
+              name: "new collection"
+            },
+            refetchQueries: [{ query: GetMyProfileDocument }]
+          })
+        }
+      >
+        create collection
+      </button>
     </div>
   );
 };
