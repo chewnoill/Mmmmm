0da9055959f4b8e25203688fd2e0ab378c55d45c
will cohen <willc@mojotech.com>
2020-04-18 12:32:14 -0400

    [client] restyle main page

--- packages/client/src/pages/main.tsx
+++ packages/client/src/pages/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import styled from "@emotion/styled";
 import {
   useGetMyProfileQuery,
   useCreateCollectionMutation,
   GetMyProfileDocument
 } from "codegen";
 import { Link } from "react-router-dom";
+import { Div } from "ui";
+
+const Page = styled(Div)`
+  padding: 15px;
+  header {
+    margin-bottom: 10px;
+  }
+  .collection-list {
+    display: flex;
+    flex-direction: column;
+  }
+`;
 
 export default () => {
   const { data } = useGetMyProfileQuery();
@@ -12,23 +25,27 @@ export default () => {
   if (!data || !data.me || !data.me.user) return <div>loading...</div>;
 
   return (
-    <div>
-      <div>hello {data.me.user.email}</div>
-      {data.me.user.collections.edges.map(({ edge: { id, name } }) => (
-        <Link to={`/collection/${id}`}>{name}</Link>
-      ))}
-      <button
-        onClick={() =>
-          createCollection({
-            variables: {
-              name: "new collection"
-            },
-            refetchQueries: [{ query: GetMyProfileDocument }]
-          })
-        }
-      >
-        create collection
-      </button>
-    </div>
+    <Page>
+      <header>
+        <div>hello {data.me.user.email}</div>
+        <button
+          onClick={() =>
+            createCollection({
+              variables: {
+                name: "new collection"
+              },
+              refetchQueries: [{ query: GetMyProfileDocument }]
+            })
+          }
+        >
+          create collection
+        </button>
+      </header>
+      <div className="collection-list">
+        {data.me.user.collections.edges.map(({ edge: { id, name } }) => (
+          <Link to={`/collection/${id}`}>{name}</Link>
+        ))}
+      </div>
+    </Page>
   );
 };
